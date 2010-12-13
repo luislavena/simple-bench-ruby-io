afc12c6716b90ea437c45ca22d30739ef412b5a0
Luis Lavena <luislavena@gmail.com>
2010-12-13 19:31:45 -0300

    Added debug helpers

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
+STDERR.puts "DEBUG: Loaded Features at startup: #{$LOADED_FEATURES.length}"
 require 'boot'
+STDERR.puts "DEBUG: Loaded Features after boot: #{$LOADED_FEATURES.length}"
 
 require 'dm-core'
 require 'dm-migrations'
 
+STDERR.puts "DEBUG: Loaded Features after requires: #{$LOADED_FEATURES.length}"
+
 DataMapper.setup(:default, 'sqlite::memory:')
+
+STDERR.puts "DEBUG: Loaded Features after setup: #{$LOADED_FEATURES.length}"
